f992bd27227d22523986808d3835c33482b252a4
AjaxKronus <107383559+AjaxKronus@users.noreply.github.com>
2026-03-25 12:07:29 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style.css'; //import css file!
+
+import 'whatwg-fetch'; // Import fetch polyfill
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
